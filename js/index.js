f6787356dc0713ca2526793588daca33704fdace
artjola zalla <78443274+tjola@users.noreply.github.com>
2023-05-04 07:26:48 +0200

    js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-        if (window.innerWidth < 780) {
-          let megamenu = document.querySelectorAll(
-            ".megamenu .main-navigation .megamenu-list"
-          );
-          document.getElementById("mobile-menu").appendChild(megamenu);
-        }    
-})
-
-
-function myFunction(x) {
-    if (window.innerWidth < 780) {
-      let megamenu = document.querySelectorAll(
-        ".megamenu .main-navigation .megamenu-list"
-      );
-      document.getElementById("mobile-menu").appendChild(megamenu);
-    }
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  const barsIcon = document.querySelector(".fa-bars");
+  barsIcon.addEventListener("click", function() {
+    const megamenuList = document.querySelector(".megamenu-list");
+    this.parentNode.appendChild(megamenuList);
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
   
+const megamenu = document.querySelector('.megamenu');
+const barsIcon = document.querySelector('.fa-bars');
+
+barsIcon.addEventListener('click', function() {
+  megamenu.classList.toggle('active');
+});
+
+});

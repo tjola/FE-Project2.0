805cadc4cb6b687f7ba304f8f5cf68b401327b78
artjola zalla <78443274+tjola@users.noreply.github.com>
2023-05-07 22:09:40 +0200

    js mobile final

--- js/index.js
+++ js/index.js
@@ -2,15 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const barsIcon = document.querySelector(".fa-bars");
   barsIcon.addEventListener("click", function () {
     const megamenuList = document.querySelector(".megamenu-list");
+    barsIcon.classList.toggle("active");
+    megamenuList.classList.toggle("active");
     this.parentNode.appendChild(megamenuList);
   });
 });
 document.addEventListener("DOMContentLoaded", function () {
   const megamenu = document.querySelector(".megamenu");
   const barsIcon = document.querySelector(".fa-bars");
+  const mobileHeader = document.querySelector(".page-titles-list");
+  const overlayMobile = document.querySelector(".overlay");
 
   barsIcon.addEventListener("click", function () {
     megamenu.classList.toggle("active");
+    mobileHeader.classList.toggle("active");
+    overlayMobile.classList.toggle("active");
+    setTimeout(() => {
+      if (barsIcon.classList.contains("active")) {
+        barsIcon.classList.remove("fa-bars");
+        barsIcon.classList.add("fa-xmark");
+      } else {
+        barsIcon.classList.remove("fa-xmark");
+        barsIcon.classList.add("fa-bars");
+      }
+    }, 300); // Use the same duration as the CSS transition (0.3 seconds)
   });
 });
-

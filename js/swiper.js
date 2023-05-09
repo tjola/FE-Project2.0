11ffda464614f48583a88c7e993255ddb9d97188
Artjola Zalla <artjola@Artjolas-iMac.local>
2023-05-09 15:09:16 +0200

    lower footer fixes

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+
+var swiper = new Swiper(".swiper-container", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  spaceBetween: '5%',
+  slidesPerView: 3,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});

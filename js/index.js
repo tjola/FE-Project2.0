4dfade9f1104d3e1fd3a53133c2077ee882f7caf
Artjola Zalla <artjola@Artjolas-iMac.local>
2023-05-09 17:21:47 +0200

    additional changes

--- js/index.js
+++ js/index.js
@@ -6,29 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
     megamenuList.classList.toggle("active");
     this.parentNode.appendChild(megamenuList);
   });
-
 });
 document.addEventListener("DOMContentLoaded", function () {
   const megamenu = document.querySelector(".megamenu");
+  const megamenuList = document.querySelector(".megamenu-list");
   const barsIcon = document.querySelector(".fa-bars");
   const mobileHeader = document.querySelector(".page-titles-list");
   const overlayMobile = document.querySelector(".overlay");
+  const linkSelected = document.querySelector(".menu-item-3 .nested");
+  const tattoLink = document.querySelector(".menu-item-3");
+  const nestedMenu = document.querySelector(".sub-menu.level-2");
+
+  linkSelected.addEventListener("click", function () {
+    nestedMenu.classList.toggle("active");
+    tattoLink.classList.toggle("active");
+  });
 
   barsIcon.addEventListener("click", function () {
     megamenu.classList.toggle("active");
     mobileHeader.classList.toggle("active");
     overlayMobile.classList.toggle("active");
+
     setTimeout(() => {
       if (barsIcon.classList.contains("active")) {
         barsIcon.classList.remove("fa-bars");
         barsIcon.classList.add("fa-xmark");
-      
       } else {
         barsIcon.classList.remove("fa-xmark");
         barsIcon.classList.add("fa-bars");
-        
       }
-    }, 300); 
+    }, 300);
   });
-
 });

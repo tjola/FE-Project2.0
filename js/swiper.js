3759a420e8f57248d919db7e4b7e54ec49a9cd4b
Artjola Zalla <artjola@Artjolas-iMac.local>
2023-05-10 10:43:37 +0200

    additional changes, new img

--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,15 @@
-
 var swiper = new Swiper(".swiper-container", {
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  spaceBetween: '5%',
-  slidesPerView: 3,
+  slidesPerView: 2,
+  lazy: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
   },
+  removeClippedSubviews: false,
 });
